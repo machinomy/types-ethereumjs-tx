f60c9d3f4356c458b1c54e149e1f63a5c0fbf26e
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-05-16 17:54:00 +0300

    Even more liberal Tx definition: include BigNumber

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,22 @@
-declare module "ethereumjs-tx" {
-  import { Buffer } from "safe-buffer";
+declare module 'ethereumjs-tx' {
+  import { Buffer } from 'safe-buffer'
+  import * as BigNumber from 'bignumber.js'
 
   class Tx {
-    constructor (raw: Buffer|Tx.TransactionProperties);
-    sign(privateKey: Buffer): void;
-    serialize(): string;
+    constructor (raw: Buffer|Tx.TransactionProperties)
+    sign(privateKey: Buffer): void
+    serialize(): string
   }
 
   namespace Tx {
     interface TransactionProperties {
-      nonce?: Buffer|number|string
-      gasPrice?: Buffer|number|string
-      gasLimit?: Buffer|number|string
-      gas?: Buffer|number|string
-      to?: Buffer|string
-      value?: Buffer|string|number
-      data?: Buffer|string
+      nonce?: Buffer | number | string
+      gasPrice?: Buffer | BigNumber.BigNumber | number | string
+      gasLimit?: Buffer | BigNumber.BigNumber | number | string
+      gas?: Buffer | BigNumber.BigNumber | number | string
+      to?: Buffer | string
+      value?: Buffer | string | number
+      data?: Buffer | string
       v?: Buffer | number | string
       r?: Buffer | number | string
       s?: Buffer | number | string
